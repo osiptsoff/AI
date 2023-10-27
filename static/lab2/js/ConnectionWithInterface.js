ba4d135dd9700ce897a98d40fcc6aaf18142f1d8
osiptsoff <osipczovnikita@gmail.com>
2023-10-28 02:17:50 +0300

    implemented interface connectivity

--- static/lab2/js/ConnectionWithInterface.js
+++ static/lab2/js/ConnectionWithInterface.js
@@ -1,50 +1,18 @@
-//import {State} from "./State.mjs";
-//import {stateFinder, dfsTraverseStep, bfsTraverseStep} from "./StateFinder.mjs";
-import {setMatrixValues, swap, getAlgorithm, valuesEnd} from "../../commonjs/InterfaceFunctions.mjs";
-import {valuesBegin, matrix, emptyNum, menuAlgorithm} from "../../commonjs/InterfaceFunctions.mjs";
+import {State} from "../../commonjs/math/State.mjs";
+import {heuristicStateFinder, manhattanDistance, misplacedNumCounter}
+    from "../../commonjs/math/heuristicSearch/HeuristicStateFinder.mjs";
+import {logger} from "../../commonjs/Logger.mjs";
+import {setMatrixValues, swap, getAlgorithm} from "../../commonjs/InterfaceFunctions.mjs";
+import {valuesEnd, valuesBegin, matrix, emptyNum, menuAlgorithm} from "../../commonjs/InterfaceFunctions.mjs";
 import {buttonAuto, buttonStep, buttonReset} from "../../commonjs/InterfaceFunctions.mjs";
 
 let start = new State(valuesBegin, emptyNum, 3);
 let finish = new State(valuesEnd, emptyNum, 3);
-
-import {
-    heuristicStateFinder,
-    manhattanDistance,
-    misplacedNumCounter
-} from "../../commonjs/math/heuristicSearch/HeuristicStateFinder.mjs";
-import {State} from "../../commonjs/math/State.mjs";
-
-heuristicStateFinder.startState = start;
-heuristicStateFinder.finishState = finish;
-heuristicStateFinder.setHeuristics(manhattanDistance);
-
-let ctr = 0;
-for(let state of heuristicStateFinder) {
-    ctr++;
-    //console.log(state);
-    if (heuristicStateFinder.statesEqual(state, finish)) {
-        console.log(state);
-        console.log(ctr);
-        break;
-    }
-}
-
-heuristicStateFinder.setHeuristics(misplacedNumCounter);
-
-ctr = 0;
-for(let state of heuristicStateFinder) {
-    ctr++;
-    //console.log(state);
-    if (heuristicStateFinder.statesEqual(state, finish)) {
-        console.log(state);
-        console.log(ctr);
-        break;
-    }
-}
-
+let stateFinder = heuristicStateFinder;
 
 let algorithm;
 let iteration;
+let fileNameAlgorithm, fileNamePath;
 let iterator;
 
 buttonAuto.onclick = startAuto;
@@ -52,28 +20,25 @@ buttonStep.onclick = startManual;
 buttonReset.onclick = reset;
 
 let manualStarted = false;
-/*
+
 function reset() {
     setMatrixValues(valuesBegin);
     outWindow.value = "";
-
     iteration = 1;
+
     // unblock alg initialize
     menuAlgorithm.forEach(b => b.disabled = false);
-
     manualStarted = false;
     buttonStep.onclick = startManual;
 }
 
 function startManual() {
     reset();
-
     manualStarted = true;
 
     initializeAlgorithm();
     // block alg initialize
     menuAlgorithm.forEach(b => b.disabled = true);
-    fileName = defineAndUseAlgorithmLogger();
     buttonStep.onclick = singleStep;
 
     singleStep();
@@ -86,16 +51,18 @@ function finishManual() {
 }
 
 function startAuto() {
+    defineAndUseAlgorithmLogger();
     if(!manualStarted) {
         initializeAlgorithm();
         // block alg initialize
-        fileName = defineAndUseAlgorithmLogger();
         reset();
         menuAlgorithm.forEach(b => b.disabled = true);
-    } else finishManual();
+    } else {
+        logger.addToBuffer(outWindow.value);
+        finishManual();
+    }
 
     menuAlgorithm.forEach(b => b.disabled = true);
-
     buttonAuto.disabled = true;
     buttonStep.disabled = true;
     buttonReset.disabled = true;
@@ -113,48 +80,136 @@ function finishAuto() {
     menuAlgorithm.forEach(b => b.disabled = false);
 }
 
-//Изменить на актуальные алгоритмы в return
-function defineAndUseAlgorithm(){
+//individual
+function defineAndUseHeuristics() {
     algorithm = getAlgorithm();
     if (algorithm === "0") {
-        return dfsTraverseStep;
+        return misplacedNumCounter;
     } else {
-        return bfsTraverseStep;
+        return manhattanDistance;
     }
 }
 
+//individual
 function initializeAlgorithm() {
-    const result = defineAndUseAlgorithm();
-    let st = new State(valuesBegin, 9, 3);
-    stateFinder.algorithm = result;
+    stateFinder.startState = start;
+    stateFinder.finishState = finish;
 
-    stateFinder.startState = st;
-    stateFinder.clear();
+    stateFinder.setHeuristics(defineAndUseHeuristics());
 
-    iterator = stateFinder[Symbol.iterator](); 
+    iterator = stateFinder[Symbol.iterator]();
 }
-*/
 
-function defineAndUseAlgorithmLogger(){
+//individual
+//rewrite
+function defineAndUseAlgorithmLogger() {
     algorithm = getAlgorithm();
     if (algorithm === "0") {
-        return logger.logfileName.h1AutoLog;
+        fileNameAlgorithm = logger.logfileName.dfsAutoLog;
+        fileNamePath = logger.logfileName.dfsRightPath;
     } else {
-        return logger.logfileName.h2AutoLog;
+        fileNameAlgorithm = logger.logfileName.bfsAutoLog;
+        fileNamePath = logger.logfileName.bfsRightPath;
     }
 }
 
-/*
-let finished = false;
-function autoAlgorithm(){
+//individual
+function serializeState(state) {
+    let res = '',
+        parent = state[stateFinder.parentSymbol],
+        children = state[stateFinder.childrenSymbol],
+        visited = state[stateFinder.visitedSymbol];
+    let parentSerialized = parent === undefined ? 'нет\n' : parent + '',
+        childrenSerialized = !children.length ? 'нет\n'  : children.join('\n'),
+        visitedSerialized = !visited.length ? 'нет\n' : visited.join('\n');
 
+    res += 'Глубина: ' + state.depth + '\n';
+    res += 'Значение эвристической характеристики: ' + state[stateFinder.heuristicsValueSymbol];
+    res += '\nРодитель:\n' + parentSerialized + '\n';
+    res += 'Текущее состояние:\n' + state + '\n';
+    res += 'Запланированные к дальнейшему посещению потомки:\n' + childrenSerialized + '\n';
+    res += 'Потомки, которые не будут посещены, и посещённые ранее состояния:\n' + visitedSerialized + '\n';
+
+    return res;
 }
 
+function visualizeRightWay(state, startTime) {
+    logger.flushBuffer(fileNameAlgorithm);
+    setMatrixValues(valuesBegin);
+    outWindow.value += "Алгоритм достиг конечного состояния!!!\nКонечное состояние найдено на глубине " + state.depth + ".\nТекущее состояние:\n" + state
+        + "\nИнформацию об итерациях и найденный путь можно посмотреть в log-файлах.\n";
+    //Правильный путь
+    let rightWay = [];
+    rightWay.push(state);
+
+    for(let i=state[stateFinder.parentSymbol]; i !== undefined; i=i[stateFinder.parentSymbol]){
+        rightWay.push(i);
+    }
+    rightWay.reverse();
+
+    logger.addToBuffer("Найденный путь\n");
+    rightWay.forEach((state) => {
+        logger.addToBuffer(state + '\n');
+    });
+    logger.flushBuffer(fileNamePath); //алгоритм записывается сюда, хотя не должен (в этом файле только путь нужен)
+
+    const endTime = performance.now();
+    const executionTime = endTime - startTime;
+    outWindow.value += "\nКоличество пройденных вершин: " + (iteration - 1)
+        + "\nВремя выполнения алгоритма: " + executionTime + " миллисекунд";
+
+    rightWay.forEach((state, index) => {
+        let changeCause = state[stateFinder.changeCauseSymbol];
+        if (changeCause) {
+            setTimeout(() => {
+                swap(changeCause[1], changeCause[0], matrix);
+                if(index === rightWay.length - 1)
+                    finishAuto();
+            }, index * 300); // 300 миллисекунд (0.3 секунды) задержки между каждым вызовом
+        }
+    });
+}
+
+let finished = false;
+
+function autoAlgorithm() {
+    const startTime = performance.now(); // performance.now() имеет высокую точность
+    if(!finished)
+        stepAlg()
+            .then(e => {
+                logger.addToBuffer("Итерация №" + (iteration++) + '\n' + serializeState(e.value));
+                return e.value
+            })
+            .then(e => {
+                if(stateFinder.statesEqual(e, finish)) {
+                    finished = true;
+                    visualizeRightWay(e, startTime);
+                }
+            })
+            .then(autoAlgorithm)
+}
 
 async function stepAlg() {
     return iterator.next();
 }
 
 function singleStep() {
-
-}*/
+    stepAlg()
+        .then(e => {
+            outWindow.value += "Итерация №" + (iteration++) + '\n' + serializeState(e.value)
+            return e.value;
+        })
+        .then(e => {
+            let changeCause = e[stateFinder.changeCauseSymbol];
+            if(changeCause) {
+                setMatrixValues(e[stateFinder.parentSymbol].matrix);
+                swap(changeCause[1], changeCause[0], matrix);
+            }
+            return e;
+        }).then(e => {
+        if(stateFinder.statesEqual(e, finish)) {
+            outWindow.value += "Конечное состояние достигнуто.\n";
+            finishManual();
+        }
+    });
+}
